7316f5e8a31fc9d335d55a2b0fdde1d89cf128b9
Arūnas Dovainis <arunas.dovainis@gmail.com>
2021-07-23 08:01:32 +0300

    Sprendinys 13

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
-const nPointsQuery = document.querySelectorAll('button'); //[data-points="1"]
-const nKomandaQuery = document.querySelectorAll('.komanda'); //[data-komanda="namu"]
+const pointsQuery = document.querySelectorAll('button'); //[data-points="1"]
+const komandaQuery = document.querySelectorAll('.lenta > .rezultatas'); //[data-komanda="namu"]
 const lentaQuery = document.querySelectorAll('.lenta > div');
-const rezultatasQuery = document.querySelectorAll('rezultatas');
+const rezultatasQuery = document.querySelectorAll('div > .rezultatas');
 
-for (let i of nPointsQuery) {
+for (let i of pointsQuery) {
     i.addEventListener('click', function () {
         console.log(i.dataset.points);
+        //console.log(i.dataset.komanda);
 
-        if (i.dataset.komanda === 'namu')
-            nKomandaQuery[0].innerText = parseInt(nKomandaQuery[0].innerText) + parseInt(i.dataset.points);
-        else if (i.dataset.komanda === 'sveciu')
-            nKomandaQuery[1].innerText = parseInt(nKomandaQuery[1].innerText) + parseInt(i.dataset.points);
 
-        console.log('"button" kintamieji yra: ' + nPointsQuery.length);
+        if (i.dataset.komanda === 'namu') {
+            komandaQuery[0].innerText = parseInt(komandaQuery[0].innerText) + parseInt(i.dataset.points);
+            console.log(komandaQuery[0].innerText);
+        }
+        else if (i.dataset.komanda === 'sveciu') {
+            komandaQuery[1].innerText = parseInt(komandaQuery[1].innerText) + parseInt(i.dataset.points);
+            console.log(komandaQuery[1].innerText);
+        }
+
+        console.log('"button" kintamieji yra: ' + pointsQuery.length);
 
         if (i.dataset.komanda === 'namu')
             console.log("Komanda " + i.dataset.komanda);
@@ -23,7 +29,7 @@ for (let i of nPointsQuery) {
 
 
 
-        console.log(nKomandaQuery);//[0].textContent + ' : ' + rezultatasQuery[1].textContent);
+        //console.log(komandaQuery);//[0].textContent + ' : ' + rezultatasQuery[1].textContent);
 
     });
 }
